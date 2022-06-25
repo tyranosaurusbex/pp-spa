b38c447657903b48d2e7f5fe7ddbffd5fc50d94c
Rebecca <rebeccacmdouglas@gmail.com>
2022-06-25 16:30:13 +0100

    added header to landing page, added material, removed boilerplate angular html

---
+++ user-registration/src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
